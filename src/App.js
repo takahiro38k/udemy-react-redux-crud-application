58704ea822eeecb77d9bf1e76a364cfa55611d91
takahiro38k <grandeur38k@gmail.com>
2019-12-16 11:12:45 +0900

    create why-jsx tag h1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     return <div>Hello, world!</div>
+//   }
+// }
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>
+    return <h1>Hello, world!</h1>
   }
 }
-
 export default App;
