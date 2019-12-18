5d9abe24bacaf974f2cba9d3d1a06eb8914197ea
takahiro38k <grandeur38k@gmail.com>
2019-12-19 08:23:46 +0900

    Learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ import React, { Component } from 'react';
 // }
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>
+    // const greeting = "Hi, Tom!"
+    // // javascriptの予約ごとかぶるので、classではなくclassNameとする。
+    // // 変数は｛｝で囲む。　※波括弧の中にjavascriptを記述できるということ。
+    // const dom = <h1 className="foo">{greeting}</h1>
+    // return dom;
+
+    // // 属性の書き方。｛｝の中にjavascriptを記述。
+    // return <input type="text" onClick={() => {console.log("I am clicked.")}} />;
+
+    // タグのforはhtmlForと書く。
+    // Reactの制約：　returnで返すJSXは1つのタグでなければならない。
+    // 複数のタグを囲む場合、React.Fragmentを使うと余計なタグを作らずに済む。
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </React.Fragment>
+    )
   }
 }
 export default App;
