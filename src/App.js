f86915c641a9bf25195cc21aaf6e6b3a508ef6f7
takahiro38k <grandeur38k@gmail.com>
2019-12-20 11:59:49 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import React from 'react';
 
-// // クラスコンポーネント
-// class App extends Component {
-//   render() {
-//     // const greeting = "Hi, Tom!"
-//     // // javascriptの予約ごとかぶるので、classではなくclassNameとする。
-//     // // 変数は｛｝で囲む。　※波括弧の中にjavascriptを記述できるということ。
-//     // const dom = <h1 className="foo">{greeting}</h1>
-//     // return dom;
-
-//     // // 属性の書き方。｛｝の中にjavascriptを記述。
-//     // return <input type="text" onClick={() => {console.log("I am clicked.")}} />;
-
-//     // タグのforはhtmlForと書く。
-//     // Reactの制約：　returnで返すJSXは1つのタグでなければならない。
-//     // 複数のタグを囲む場合、React.Fragmentを使うと余計なタグを作らずに済む。
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//       </React.Fragment>
-//     )
-//   }
+// const App = () => {
+//   return (
+//     <div>
+//       <User name={"Taro"} age={10}/>
+//       <User name={"Hanako"} age={5}/>
+//     </div>
+//   )
 // }
 
-// ファンクショナルコンポーネント
+// const User = (props) => {
+//   return <div>HI, I am {props.name}, and {props.age} years old!</div>
+// }
+
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Hanako", age: 5},
+    { name: "NoName" }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        // 下記エラーを出さないためにindexを設定。
+        // Warning: Each child in a list should have a unique "key" prop.
+        profiles.map((profiles, index) => {
+          return <User name={profiles.name} age={profiles.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>HI, I am {props.name}, and {props.age} years old!</div>
+}
+
+// デフォルト・プロップス
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
