c60c50e1717b17735857962d04ba20767098b5f3
takahiro38k <grandeur38k@gmail.com>
2019-12-24 16:44:09 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// storeを作成するための関数。
+import { createStore } from 'redux';
+// 作成したstoreを全コンポーネントに渡す機能を持つ特殊なコンポーネント。
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+// コードを見やすいよう、フォルダを作成して移動。
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-// Reducerを結合するためにインポート。
+// 全Reducerを1つに結合するための関数をimport。
 import { combineReducers } from 'redux'
+// stateを持つcountというReducerをimport
 import count from './count'
 
+// storeを作成するためにexport。
+// Reducerが複数ある場合は{}内で,区切りで列挙可能。
 export default combineReducers({ count })
